a4a0a2dea81d951348feafe9583c1cbcd0c6d39e
Christian Voß <cech12@gmail.com>
2021-12-04 09:00:37 +0100

    Update mod to Forge 1.18-38.0.4

--- src/main/java/cech12/woodenhopper/api/inventory/WoodenHopperContainerTypes.java
+++ src/main/java/cech12/woodenhopper/api/inventory/WoodenHopperContainerTypes.java
@@ -6,13 +6,13 @@ import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
-import net.minecraftforge.common.extensions.IForgeContainerType;
+import net.minecraftforge.common.extensions.IForgeMenuType;
 
 public class WoodenHopperContainerTypes {
 
     public final static ResourceLocation WOODEN_HOPPER_ID = new ResourceLocation(WoodenHopperMod.MOD_ID, "woodenhopper");
 
-    public static MenuType<? extends AbstractContainerMenu> WOODEN_HOPPER = IForgeContainerType.create((pWindowID, pInventory, pData) -> {
+    public static MenuType<? extends AbstractContainerMenu> WOODEN_HOPPER = IForgeMenuType.create((pWindowID, pInventory, pData) -> {
         BlockPos pos = pData.readBlockPos();
         return new WoodenHopperContainer(pWindowID, pInventory, pos);
     }).setRegistryName(WOODEN_HOPPER_ID);
--- src/main/java/cech12/woodenhopper/block/WoodenHopperBlock.java
+++ src/main/java/cech12/woodenhopper/block/WoodenHopperBlock.java
@@ -28,7 +28,7 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.fmllegacy.network.NetworkHooks;
+import net.minecraftforge.network.NetworkHooks;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
